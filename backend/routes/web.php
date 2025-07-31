0a6ee221d0c1e4508166ab765c2270b2b71e4776
Antonio <antonio@develonsystems.com.br>
2025-07-31 18:00:46 -0300

    Ajustes gerais

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,21 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('welcome');
-})->name('home');
-
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('dashboard', function () {
-        return Inertia::render('dashboard');
-    })->name('dashboard');
+    return view('welcome');
 });
 
 Route::get('/docs', function () {
     return response()->file(public_path('swagger-ui/index.html'));
 });
-
-require __DIR__ . '/settings.php';
-require __DIR__ . '/auth.php';
