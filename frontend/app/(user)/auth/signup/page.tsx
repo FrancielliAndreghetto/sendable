d91daf63743edc85a4df7b4063acf31af8fe8a68
Antonio <antonio@develonsystems.com.br>
2025-07-31 16:21:27 -0300

    ajuste front

---
+++ frontend/app/(user)/auth/signup/page.tsx
@@ -0,0 +1,25 @@
+import { memo } from "react";
+import SignupForm from "@/components/forms/SignupForm";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Signup",
+};
+
+const Signup = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <Card className="w-full max-w-sm">
+        <CardHeader>
+          <CardTitle className="text-center">Sign Up</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <SignupForm showSignIn={true} signInLink="/auth" />
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default memo(Signup);
---
+++ frontend/app/metadata.ts
@@ -0,0 +1,6 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Sendable",
+  description: "Sendable app",
+};
